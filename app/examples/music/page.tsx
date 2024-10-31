bd3574f9b8a57749d8aec5f4c2501abe832ecde4
Bytewer <dwx_job@outlook.com>
2024-11-01 00:55:34 +0800

    Update page.tsx

--- app/examples/music/page.tsx
+++ app/examples/music/page.tsx
@@ -30,7 +30,7 @@ export default function MusicPage() {
               <Sidebar playlists={playlists} className="hidden lg:block" />
               <div className="col-span-3 lg:col-span-4 lg:border-l">
                 <div className="h-full px-4  lg:px-8">
-                  <Tabs defaultValue="music" className="h-full space-y-6">
+                  <Tabs defaultValue="music" className="h-full">
                     <div className="space-between flex items-center">
                       <TabsList>
                         <TabsTrigger value="music" className="relative">
@@ -75,16 +75,16 @@ export default function MusicPage() {
                           <ScrollBar orientation="horizontal" />
                         </ScrollArea>
                       </div>
-                      <div className="mt-6 space-y-1">
+                      <div className=" ">
                         <h2 className="text-2xl font-semibold tracking-tight">Made for You</h2>
                         <p className="text-sm text-muted-foreground">
                           Your personal playlists. Updated daily.
                         </p>
                       </div>
-                      <Separator className="my-4" />
+                      <Separator className="" />
                       <div className="relative">
                         <ScrollArea>
-                          <div className="flex space-x-4 pb-4">
+                          <div className="flex  pb-4">
                             {madeForYouAlbums.map((album) => (
                               <AlbumArtwork
                                 key={album.name}
@@ -105,14 +105,14 @@ export default function MusicPage() {
                       className="h-full flex-col border-none p-0 data-[state=active]:flex"
                     >
                       <div className="flex items-center justify-between">
-                        <div className="space-y-1">
+                        <div className="">
                           <h2 className="text-2xl font-semibold tracking-tight">New Episodes</h2>
                           <p className="text-sm text-muted-foreground">
                             Your favorite podcasts. Updated daily.
                           </p>
                         </div>
                       </div>
-                      <Separator className="my-4" />
+                      <Separator className="" />
                       <PodcastEmptyPlaceholder />
                     </TabsContent>
                   </Tabs>
