a3c90fb4a8597f954ab3058edd2c0a593be64e88
Bytewer <dwx_job@outlook.com>
2024-11-01 00:38:18 +0800

    Update page.tsx

--- app/examples/playground/page.tsx
+++ app/examples/playground/page.tsx
@@ -29,24 +29,8 @@ export const metadata: Metadata = {
 export default function PlaygroundPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/playground-light.png"
-          width={1280}
-          height={916}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/playground-dark.png"
-          width={1280}
-          height={916}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-col md:flex">
-        <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+      <div className=" flex flex-col ">
+        <div className="container flex flex-col items-start justify-between sm:flex-row sm:items-center sm:space-y-0 md:h-16">
           <h2 className="text-lg font-semibold">Playground</h2>
           <div className="ml-auto flex w-full space-x-2 sm:justify-end">
             <PresetSelector presets={presets} />
@@ -60,7 +44,7 @@ export default function PlaygroundPage() {
         </div>
         <Separator />
         <Tabs defaultValue="complete" className="flex-1">
-          <div className="container h-full py-6">
+          <div className="container h-full">
             <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
               <div className="hidden flex-col space-y-4 sm:flex md:order-2">
                 <div className="grid gap-2">
