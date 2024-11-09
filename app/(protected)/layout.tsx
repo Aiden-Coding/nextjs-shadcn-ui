d1e13592449ac6204659890670a9a53ebd5bc051
Aiden Dong <dwx_job@163.com>
2024-11-09 14:18:22 +0800

    1

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -15,6 +15,7 @@ interface ExamplesLayoutProps {
 export default async function ExamplesLayout({ children }: ExamplesLayoutProps) {
   const cookieStore = await cookies();
   const sidebarStatus = cookieStore.get("sidebar:state");
+  console.log(sidebarStatus);
   const defaultSidebarOpen = sidebarStatus ? sidebarStatus.value === "true" : true;
   console.log(defaultSidebarOpen);
   return (
