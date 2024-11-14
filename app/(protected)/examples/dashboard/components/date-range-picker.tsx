df18c2b286092cb2d777e71d8a05a9f95844be97
Aiden Dong <dwx_job@163.com>
2024-11-14 21:52:27 +0800

    1

--- app/(protected)/examples/dashboard/components/date-range-picker.tsx
+++ app/(protected)/examples/dashboard/components/date-range-picker.tsx
@@ -9,20 +9,33 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { PgliteContext, PgliteContextType } from "@/components/pglite-provider";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export function CalendarDateRangePicker({ className }: React.HTMLAttributes<HTMLDivElement>) {
   const { pglite } = useContext(PgliteContext) as PgliteContextType;
 
   const [isReady, setIsReady] = useState(false);
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (pglite) {
       setIsReady(true);
       console.log("i pg suc");
     }
   }, [pglite]);
-  React.useEffect(() => {
+  useEffect(() => {
     const dff = async () => {
       if (pglite) {
         const d = await pglite.query("select 'Hello world' as message;");
@@ -33,7 +46,7 @@ export function CalendarDateRangePicker({ className }: React.HTMLAttributes<HTML
       }
     };
     dff();
-  }, []);
+  }, [pglite]);
 
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: new Date(2023, 0, 20),
@@ -41,42 +54,62 @@ export function CalendarDateRangePicker({ className }: React.HTMLAttributes<HTML
   });
 
   return (
-    <div className={cn("grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-[260px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
-                </>
-              ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="end">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
+    <>
+      {!isReady ? (
+        <AlertDialog open={!isReady}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account and remove
+                your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      ) : (
+        <div className={cn("grid gap-2", className)}>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-[260px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
+                ) : (
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="end">
+              <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+      )}
+    </>
   );
 }
